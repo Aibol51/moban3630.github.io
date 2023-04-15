1e2e9c4d2c54fd52630fac9a13767a97e143558f
Aibo <ggwp4236@gmail.com>
2023-04-15 17:34:27 +0400

    prod

--- main.js
+++ main.js
@@ -11,10 +11,23 @@ if (localStorage.getItem("users")) {
 
   // 创建用户链接元素
   const userLink = document.createElement("a");
-  userLink.href = "/user.html";
+  userLink.href = "/users.html";
   getUsers = JSON.parse(localStorage.getItem("users"));
   userLink.innerText = getUsers.email
 
   // 添加用户链接元素
   userLinks.appendChild(userLink);
+}
+
+// 判断是否已登陆
+// 获取当前页面的pathname
+let pathname = window.location.pathname;
+
+// 检查是否为login或register页面
+if (pathname.indexOf("login.html") !== -1 || pathname.indexOf("register.html") !== -1) {
+  // 检查localStorage是否存在用户登录信息
+  if (localStorage.getItem("users")) {
+    // 重定向到index.html页面
+    window.location.href = "index.html";
+  }
 }
